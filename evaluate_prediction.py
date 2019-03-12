9a4bf066db35335402f7b9523911a2743443b6d6
BeritJanssen <berit.janssen@gmail.com>
2019-03-12 11:47:24 +0100

    included conditions for empty generated and original sequences

--- evaluate_prediction.py
+++ evaluate_prediction.py
@@ -53,9 +53,6 @@ def evaluate_continuation(
     in which the key indicates the onsets after cut-off
     for which correct number of pitches, iois and pitch/ioi pairs are listed. 
     """
-
-    original_onsets = sorted(list(set([float(o['onset']) for o in original])))
-    generated_onsets = sorted(list(set([float(g['onset']) for g in generated])))
     outputlist = []
     last_onset_prime = float(last_event_prime['onset'])
     max_onset = last_onset_prime + evaluate_until_onset
@@ -67,54 +64,83 @@ def evaluate_continuation(
             generated_events = [
                 g for g in generated if float(g['onset']) <= onset
             ]
-            if not generated_events or not original_events:
-                continue
-            original_pitches = [int(o['pitch']) for o in original_events]
-            generated_pitches = [int(g['pitch']) for g in generated_events]
-            original_iois = [
-                round(
-                    float(o['onset']) - last_onset_prime, 2
-                ) for o in original_events
-            ]
-            generated_iois = [
-                round(
-                    float(g['onset']) - last_onset_prime, 2
-                ) for g in generated_events
-            ]
-            original_pairs = list(zip(original_pitches, original_iois))
-            generated_pairs = list(zip(generated_pitches, generated_iois))
-            correct_pitches_at_n = 0
-            correct_iois_at_n = 0
-            correct_pairs_at_n = 0
-            for event in generated_pitches:
-                if event in original_pitches:
-                    correct_pitches_at_n += 1
-                    original_pitches.pop(original_pitches.index(event))
-            for event in generated_iois:
-                if event in original_iois:
-                    correct_iois_at_n += 1
-                    original_iois.pop(original_iois.index(event))
-            for event in generated_pairs:
-                if event in original_pairs:
-                    correct_pairs_at_n += 1
-                    original_pairs.pop(original_pairs.index(event))
-            prec_pitch = correct_pitches_at_n / len(generated_events)
-            prec_ioi = correct_iois_at_n / len(generated_events)
-            prec_pairs = correct_pairs_at_n / len(generated_events)
-            rec_pitch = correct_pitches_at_n / len(original_events)
-            rec_ioi = correct_iois_at_n / len(original_events)
-            rec_pairs = correct_pairs_at_n / len(original_events)
+            if (len(generated_events)==0 and len(original_events)==0):
+                prec_pitch = prec_ioi = prec_pairs = 1
+                rec_pitch = rec_ioi = rec_pairs = 1
+                f1_pitch = f1_ioi = f1_pairs = 1
+                correct_pitches_at_n = correct_iois_at_n = correct_pairs_at_n = None
+            elif (len(original_events)==0 or len(generated_events)==0):
+                prec_pitch = prec_ioi = prec_pairs = 0
+                rec_pitch = rec_ioi = rec_pairs = 0
+                f1_pitch = f1_ioi = f1_pairs = 0
+                correct_pitches_at_n = correct_iois_at_n = correct_pairs_at_n = None
+            else:
+                original_pitches = [int(o['pitch']) for o in original_events]
+                generated_pitches = [int(g['pitch']) for g in generated_events]
+                original_iois = [
+                    round(
+                        float(o['onset']) - last_onset_prime, 2
+                    ) for o in original_events
+                ]
+                generated_iois = [
+                    round(
+                        float(g['onset']) - last_onset_prime, 2
+                    ) for g in generated_events
+                ]
+                original_pairs = list(zip(original_pitches, original_iois))
+                generated_pairs = list(zip(generated_pitches, generated_iois))
+                correct_pitches_at_n = 0
+                correct_iois_at_n = 0
+                correct_pairs_at_n = 0
+                for event in generated_pitches:
+                    if event in original_pitches:
+                        correct_pitches_at_n += 1
+                        original_pitches.pop(original_pitches.index(event))
+                for event in generated_iois:
+                    if event in original_iois:
+                        correct_iois_at_n += 1
+                        original_iois.pop(original_iois.index(event))
+                for event in generated_pairs:
+                    if event in original_pairs:
+                        correct_pairs_at_n += 1
+                        original_pairs.pop(original_pairs.index(event))
+                prec_pitch = correct_pitches_at_n / len(generated_events)
+                prec_ioi = correct_iois_at_n / len(generated_events)
+                prec_pairs = correct_pairs_at_n / len(generated_events)
+                rec_pitch = correct_pitches_at_n / len(original_events)
+                rec_ioi = correct_iois_at_n / len(original_events)
+                rec_pairs = correct_pairs_at_n / len(original_events)
+                if prec_pitch + rec_pitch == 0:
+                    f1_pitch = 0
+                else:
+                    f1_pitch = 2 * rec_pitch * prec_pitch / (
+                        rec_pitch + prec_pitch)
+                if prec_ioi + rec_ioi == 0:
+                    f1_ioi = 0
+                else:
+                    f1_ioi = 2 * rec_ioi * prec_ioi / (
+                        rec_ioi + prec_ioi)
+                if prec_pairs + rec_pairs == 0:
+                    f1_pairs = 0
+                else:
+                    f1_pairs = 2 * rec_pairs * prec_pairs / (
+                        rec_pairs + prec_pairs
+                    )
             outputlist.append({
                 'onset': onset - last_onset_prime,
                 'pitches_correct': correct_pitches_at_n,
                 'iois_correct': correct_iois_at_n,
                 'pitch_ioi_pairs_correct': correct_pairs_at_n,
-                'no_predicted_events': len(original_events),
+                'no_original_events': len(original_events),
+                'no_generated_events': len(generated_events),
                 'prec_pitch': prec_pitch,
                 'prec_ioi': prec_ioi,
                 'prec_pairs': prec_pairs,
                 'rec_pitch': rec_pitch,
                 'rec_ioi': rec_ioi,
-                'rec_pairs': rec_pairs
+                'rec_pairs': rec_pairs,
+                'f1_pitch': f1_pitch,
+                'f1_ioi': f1_ioi,
+                'f1_pairs': f1_pairs
             })
     return outputlist
