6aa2c670dd01aa1a57da48ea239eb0be68838546
JamesOwers <james.f.owers+github@gmail.com>
2019-09-16 11:16:26 +0100

    Added new pitch score
    This score simply calculates the overlap between normalised counts
    of the pitches for the original and generated continuations. I chose
    this method as opposed to the KL divergence between the two discrete
    distributions because using the KL would require some smoothing -
    0 values would introduce infinite values. The choice of small zero
    value would really affect algorithms that go for few or many pitches
    in their continuations.
    
    Additionally I decided not to break this score down by time, this
    could be implemented in future if you like, but I don't think it
    adds a great deal of value.

--- evaluate_prediction.py
+++ evaluate_prediction.py
@@ -19,10 +19,10 @@ import config
 
 
 def evaluate_cs(original, generated):
-    '''Given a original and generated events, calculate precision and recall of
-    the cardinality score. It is expected that `original` and `generated` are
-    pandas dataframes containing columns 'onset' and 'pitch' and that they have
-    been deduplicated.
+    '''Given a original and generated events, calculate precision, recall, and
+    F1 of the cardinality score. It is expected that `original` and `generated`
+    are pandas dataframes containing columns 'onset' and 'pitch' and that they
+    have been deduplicated.
 
     Parameters
     ----------
@@ -58,6 +58,43 @@ def evaluate_cs(original, generated):
     output = {'rec': recall, 'prec': precision, 'F1': f1}
     return output
 
+
+def evaluate_pitch_score(original, generated, ignore_octave=False):
+    '''Given a original and generated events, calculate the pitch score. It is
+    expected that `original` and `generated` are pandas dataframes containing
+    columns 'onset' and 'pitch' and that they have been deduplicated. The pitch
+    score is the overlap of the normalised histograms of the pitches.
+
+    Parameters
+    ----------
+    original : pd.DataFrame
+        A dataframe containing columns 'onset' and 'pitch' representing the
+        true continuation
+    generated : pd.DataFrame
+        A dataframe containing columns 'onset' and 'pitch' representing the
+        generated continuation to be evaluated
+
+    Returns
+    -------
+    overlap : float
+        The proportion of overlap between the normalised pitch histograms
+    '''
+    if ignore_octave is True:
+        original = original.copy()
+        original['pitch'] = original['pitch'] % 12
+        generated = generated.copy()
+        generated['pitch'] = generated['pitch'] % 12
+    orig_pitch = original.pitch.value_counts(normalize=True)
+    orig_pitch.name = 'orig_pitch'
+    gen_pitch = generated.pitch.value_counts(normalize=True)
+    gen_pitch.name = 'gen_pitch'
+    pitches = pd.concat((orig_pitch, gen_pitch), axis=1)
+    overlap = 0
+    for pitch, row in pitches.iterrows():
+         if not row.isnull().any():
+             overlap += min(row.iloc[0], row.iloc[1])
+    return overlap
+    
 
 def evaluate_continuation(original, generated, last_onset_prime,
                           onset_increment, evaluate_from_onset,
@@ -134,9 +171,9 @@ if __name__ == '__main__':
 
     print('Reading PPTD csv files')
     prime = {get_fn(path): pd.read_csv(path, names=COLNAMES)
-             for path in tqdm(glob('{}/prime_csv/*'.format(PATH)))}
+             for path in tqdm(glob(f'{PATH}/prime_csv/*'))}
     cont_true = {get_fn(path): pd.read_csv(path, names=COLNAMES)
-                 for path in tqdm(glob('{}/cont_true_csv/*'.format(PATH)))}
+                 for path in tqdm(glob(f'{PATH}/cont_true_csv/*'))}
     # preprocessing to remove duplicates
     cont_true = {fn: dedup_and_preproc(df) for fn, df in cont_true.items()}
     fn_list = list(prime.keys())
@@ -144,19 +181,20 @@ if __name__ == '__main__':
     files_dict = {}
     alg_names = config.MODEL_DIRS.keys()
     for alg in alg_names:
-        print('Reading {} output files'.format(alg))
+        print(f'Reading {alg} output files')
         alg_cont = {
             get_fn(path): pd.read_csv(path, names=config.MODEL_KEYS[alg])
-            for path in tqdm(glob('{}/*.csv'.format(config.MODEL_DIRS[alg])))
+            for path in tqdm(glob(f'{config.MODEL_DIRS[alg]}/*.csv'))
         }
         # preprocessing to remove duplicates
         alg_cont = {fn: dedup_and_preproc(df) for fn, df in alg_cont.items()}
         files_dict[alg] = alg_cont
-
-    scores = {alg: {} for alg in alg_names}
+        
+    
+    card_scores = {alg: {} for alg in alg_names}
 
     for alg in alg_names:
-        print('Scoring {} results with cardinality score'.format(alg))
+        print(f'Scoring {alg} with cardinality score')
         for fn in tqdm(fn_list):
             # the generated file name may have additions to original file name
             generated_fn = next(
@@ -167,21 +205,24 @@ if __name__ == '__main__':
             true_df = cont_true[fn]
             gen_df = files_dict[alg][generated_fn]
             prime_final_onset = prime[fn].iloc[-1]['onset']
-            scores[alg][fn] = evaluate_continuation(
+            card_scores[alg][fn] = evaluate_continuation(
                 true_df,
                 gen_df,
                 prime_final_onset,
                 0.5, 2.0, 10.0
             )
+    
     df_list = []
-    for metric in ['recall', 'precision', 'F1']:
-        for key in scores.keys():
-            data = {fn: scores[key][fn][metric] for fn in fn_list}
+    for measure in ['recall', 'precision', 'F1']:
+        metric = 'cardinality'
+        for key in card_scores.keys():
+            data = {fn: card_scores[key][fn][measure] for fn in fn_list}
             df = (pd.DataFrame
                     .from_dict(data, orient='index')
                     .reset_index()
                     .rename(columns={'index': 'fn'})
-                    .melt(id_vars=['fn'], var_name='t', value_name='score')
+                    .melt(id_vars=['fn'], var_name='t',
+                          value_name=f'{metric} score')
             )
             df['model'] = key
             df_list.append(df)
@@ -189,7 +230,7 @@ if __name__ == '__main__':
         sns.set_style("whitegrid")
         g = sns.lineplot(
             x='t',
-            y='score',
+            y=f'{metric} score',
             hue='model',
             hue_order=config.MODEL_DIRS.keys(),
             style='model',
@@ -197,7 +238,94 @@ if __name__ == '__main__':
             markers=['o', 'v', 's'],
             data=pd.concat((df_list), axis=0)
         )
-        g.set(xlabel='Onset', ylabel=str.title(metric))
-        plt.title('Comparison of models on {} metric'.format(metric))
-        filename = metric+".png"
+        g.set(xlabel='Onset', ylabel=str.title(measure))
+        plt.title(f'Comparison by {measure} of {metric} score')
+        filename = f"{metric}_score__{measure}.png"
         plt.savefig(filename, dpi=300)
+
+
+    pitch_scores = {alg: {} for alg in alg_names}
+    pitch_scores_nooctave = {alg: {} for alg in alg_names}
+    for alg in alg_names:
+        print(f'Scoring {alg} with pitch score')
+        for fn in tqdm(fn_list):
+            # the generated file name may have additions to original file name
+            generated_fn = next(
+                (alg_fn for alg_fn in files_dict[alg].keys()
+                 if re.search(fn, alg_fn)),
+                None
+            )
+            true_df = cont_true[fn]
+            gen_df = files_dict[alg][generated_fn]
+            pitch_scores[alg][fn] = evaluate_pitch_score(
+                true_df,
+                gen_df
+            )
+            pitch_scores_nooctave[alg][fn] = evaluate_pitch_score(
+                true_df,
+                gen_df,
+                ignore_octave=True
+            )
+            
+    metric = 'pitch'
+    df_list = []
+    pitch_score_table = pd.DataFrame(columns=['mean', 'median', 'sd'],
+                                     dtype=float)
+    pitch_score_table.index.name = 'alg'
+    for key in pitch_scores.keys():
+        data = {fn: pitch_scores[key][fn] for fn in fn_list}
+        df = (
+            pd.DataFrame
+                .from_dict(data, orient='index')
+                .reset_index()
+                .rename(columns={'index': 'fn', 0: 'pitch_score'})
+        )
+        df['model'] = key
+        df_list.append(df)
+        pitch_score_table.loc[key, :] = [df.pitch_score.mean(),
+                                           df.pitch_score.median(),
+                                           df.pitch_score.std()]
+    df = pd.concat(df_list, axis=0)
+    plt.figure()
+    sns.set_style("whitegrid")
+    sns.violinplot(x='model', y='pitch_score', data=df, ax=plt.gca(),
+                   scale='width', bw=.1, cut=0)
+    plt.title(f'Comparison of {metric} score')
+    filename = f"{metric}_score.png"
+    plt.savefig(filename, dpi=300)
+    rounded_pitch_score_table = pitch_score_table.round(decimals=3)
+    print(rounded_pitch_score_table)
+    rounded_pitch_score_table.to_html(f'{metric}_score_table.html')
+    rounded_pitch_score_table.to_latex(f'{metric}_score_table.tex')
+    
+    
+    metric = 'pitch_no_octave'
+    df_list = []
+    pitch_score_table = pd.DataFrame(columns=['mean', 'median', 'sd'],
+                                     dtype=float)
+    pitch_score_table.index.name = 'alg'
+    for key in pitch_scores_nooctave.keys():
+        data = {fn: pitch_scores_nooctave[key][fn] for fn in fn_list}
+        df = (
+            pd.DataFrame
+                .from_dict(data, orient='index')
+                .reset_index()
+                .rename(columns={'index': 'fn', 0: 'pitch_score'})
+        )
+        df['model'] = key
+        df_list.append(df)
+        pitch_score_table.loc[key, :] = [df.pitch_score.mean(),
+                                         df.pitch_score.median(),
+                                         df.pitch_score.std()]
+    df = pd.concat(df_list, axis=0)
+    plt.figure()
+    sns.set_style("whitegrid")
+    sns.violinplot(x='model', y='pitch_score', data=df, ax=plt.gca(),
+                   scale='width', bw=.1, cut=0)
+    plt.title(f'Comparison of {metric} score')
+    filename = f"{metric}_score.png"
+    plt.savefig(filename, dpi=300)
+    rounded_pitch_score_table = pitch_score_table.round(decimals=3)
+    print(rounded_pitch_score_table)
+    rounded_pitch_score_table.to_html(f'{metric}_score_table.html')
+    rounded_pitch_score_table.to_latex(f'{metric}_score_table.tex')
